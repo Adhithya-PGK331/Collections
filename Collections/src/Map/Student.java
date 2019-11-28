57417e4b2bef79cb0d5db7a537c26666b6122231
USER <USER@FACE-71>
2019-11-28 10:56:34 +0530

    Student-details

---
+++ Collections/src/Map/Student.java
@@ -0,0 +1,61 @@
+package Map;
+
+public class Student {
+
+	String name;
+	int age;
+	int regno;
+	int phoneno;
+	String cname,address;
+	int mark;
+	int year;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getRegno() {
+		return regno;
+	}
+	public void setRegno(int regno) {
+		this.regno = regno;
+	}
+	public int getPhoneno() {
+		return phoneno;
+	}
+	public void setPhoneno(int phoneno) {
+		this.phoneno = phoneno;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getMark() {
+		return mark;
+	}
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+	
+}
--- Collections/src/Map/Student_details.java
+++ Collections/src/Map/Student_details.java
@@ -3,50 +3,70 @@ package Map;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Map.Entry;
 
 public class Student_details {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner s=new Scanner(System.in);
-		System.out.println("Enter no of students");
-		int n=s.nextInt();
-		Student[]stud=new Student[n];
-		
-		int i=0;
-		Map<Integer,Employee> map=new HashMap<Integer,Employee>();
-		for(i=0;i<n;i++)
-		{
-		
-		stud[i]=new Student();
-		System.out.println("Enter your name");
-		String name=s.nextLine();
-		System.out.println("Enter your age");
-		int age=s.nextInt();
-		System.out.println("Enter your Register number");
-		int regno=s.nextInt();
-		System.out.println("Enter your Phone number");
-		int phoneno=s.nextInt();
-		System.out.println("Enter your College name");
-		int cname=s.nextInt();
-		System.out.println("Enter your Address");
-		int address=s.nextInt();
-		System.out.println("Enter your mark");
-		int mark=s.nextInt();
-		System.out.println("Enter your year");
-		int year=s.nextInt();
-		map.put(i, stud[i]);
-			}
-		for(Map.Entry<Integer, Employee>en:map.entrySet())
-		{
-			int key=en.getKey();
-			Employee e=en.getValue();
-			System.out.println(key+" "+e.getName()+" "+e.getAge()+key+" "+e.getRegno()+" "key+" "+e.getPhoneno+" "+e.getCname+" "+e.getAddress+" "+e.getMark+" "+e.getYear);
-			}
-	}
-
-}
-
-class Student{
+	Scanner s=new Scanner(System.in);
+	System.out.println("Enter no of students");
+	int n=s.nextInt();
+	Student[]stud=new Student[n];
 	
+	int i=0;
+	Map<Integer,Student> map=new HashMap<Integer,Student>();
+	for(i=0;i<n;i++)
+	{
+	
+	stud[i]=new Student();
+	System.out.println("Enter your name");
+	String name=s.next();
+	System.out.println("Enter your age");
+	int age=s.nextInt();
+	System.out.println("Enter your Register number");
+	int regno=s.nextInt();
+	System.out.println("Enter your Phone number");
+	int phoneno=s.nextInt();
+	System.out.println("Enter your College name");
+	String cname=s.next();
+	System.out.println("Enter your Address");
+	String address=s.next();
+	System.out.println("Enter your mark");
+	int mark=s.nextInt();
+	System.out.println("Enter your year");
+	int year=s.nextInt();
+	
+	stud[i].setName(name);
+	stud[i].setAge(age);
+	stud[i].setRegno(regno);
+	stud[i].setPhoneno(phoneno);
+	stud[i].setCname(cname);
+	stud[i].setAddress(address);
+	stud[i].setMark(mark);
+	stud[i].setYear(year);
+	map.put(i, stud[i]);
+	
+	}
+	System.out.println("Enter the key :\n");
+	int k=s.nextInt();
+	
+for(Entry<Integer, Student> en:map.entrySet())
+{
+	int flag=0;
+	Student e=null;int key=0;
+	for(i=0;i<n;i++) {
+	key=en.getKey();
+	e=en.getValue();
+	key=e.getRegno();
+	if(k==key) {
+		flag=1;break;
+	}
+	else {
+		flag=0;
+	}
+	}
+	if(flag==1) {
+	System.out.println(key+" "+e.getName()+" "+e.getAge()+" "+e.getRegno()+" "+e.getPhoneno()+" "+e.getCname()+" "+e.getAddress()+" "+e.getMark()+" "+e.getYear());
+			}
+		}
+	}
 }
